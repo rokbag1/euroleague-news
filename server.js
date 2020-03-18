919b4faf8984deafcc0fd68fc3b69a630c2070f4
Rokas <38216453+rokbag1@users.noreply.github.com>
2020-03-18 18:02:00 +0200

    Fixes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-
 require('dotenv/config');
 
 //app.use
